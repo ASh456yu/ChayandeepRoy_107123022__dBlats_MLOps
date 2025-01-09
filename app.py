b0c0155f6b374cb5bda9243b89ada4379195472a
Anonymous Coder <cdroy36907@gmail.com>
2025-01-09 23:49:54 +0530

    3rd commit

--- app.py
+++ app.py
@@ -30,31 +30,29 @@ app = Flask(__name__, template_folder='templates')
 def index():
     return render_template('index.html')
 
+
+
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        
         image_file = request.files['image_file']
         model_name = request.form.get('model')
+
         if not image_file:
-            return jsonify({'error': "No image given"})
+            return jsonify({'error': "No image given"}), 400
         if not model_name:
             return jsonify({'error': 'No model specified'}), 400
-        
+
         image = Image.open(image_file).convert('RGB')
         image = image.resize((256, 256))
         image_array = np.array(image)
         image_array = np.expand_dims(image_array, axis=0)
 
-
-
         result = model.predict(image_array)
 
-        if result > 0.5:
-            prediction = "dog"
-        else:
-            prediction = "cat"
+        prediction = "dog" if result > 0.5 else "cat"
         return jsonify({'result': prediction}), 200
+
     except ValueError as e:
         return jsonify({'error': str(e)}), 400
     except Exception as e:
